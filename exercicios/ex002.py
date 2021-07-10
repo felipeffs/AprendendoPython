bdd11bb130771acfd64204115451299a4deeba4d
Felipe Ferreira <felipe8ffs@gmail.com>
2021-07-10 14:03:49 -0300

    Enunciado do ex002 adicionado

--- exercicios/ex002.py
+++ exercicios/ex002.py
@@ -1,2 +1,3 @@
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 nome = input('Qual o seu nome? ')
 print('Olá {}! Tenha um ótimo dia, desgraça!'.format(nome))
