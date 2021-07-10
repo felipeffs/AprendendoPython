8af283c41bca2d78d407f2884e44b3f1597dc65c
Felipe Ferreira <felipe8ffs@gmail.com>
2021-07-10 15:14:48 -0300

    Ex004 atualizado
    + Enunciado
    + Quebras de linhas para maior legibilidade

--- exercicios/ex004.py
+++ exercicios/ex004.py
@@ -1,4 +1,16 @@
+"""
+Faça um programa que leia algo pelo teclado e mostre na tela o seu
+tipo primitivo e todas as informações possíveis sobre ele.
+"""
 
 ent = input('Digite qualquer coisa: ')
 print('Armazenado como tipo {}'.format(type(ent)))
-print('\nO que digitou é Alfanumérico? {} \nÉ Alfabético? {}\nÉ Numérico? {}\nEstá somente em caixa baixa? {}\nEstá somente em caixa alta? {}\nEstá na tabela ascii? {}\nTem somente espaços? {}\nEstá Capitalizada? {}'.format(ent.isalnum(), ent.isalpha(), ent.isnumeric(), ent.islower(), ent.isupper(), ent.isascii(), ent.isspace(), ent.istitle()))
+print('\nO que digitou é Alfanumérico? {} '
+      '\nÉ Alfabético? {}'
+      '\nÉ Numérico? {}'
+      '\nEstá somente em caixa baixa? {}'
+      '\nEstá somente em caixa alta? {}'
+      '\nEstá na tabela ascii? {}'
+      '\nTem somente espaços? {}'
+      '\nEstá Capitalizada? {}'.format(ent.isalnum(), ent.isalpha(), ent.isnumeric(), ent.islower(), ent.isupper(),
+                                       ent.isascii(), ent.isspace(), ent.istitle()))
