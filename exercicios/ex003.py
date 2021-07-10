d8c39827c7f3c61e217c8f3cc4cae4638d18e1cc
Felipe Ferreira <felipe8ffs@gmail.com>
2021-07-10 15:03:38 -0300

    Ex003 atualizado
    + Enunciado
    + Pequenas alterações

--- exercicios/ex003.py
+++ exercicios/ex003.py
@@ -1,4 +1,6 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 
-print('A soma entre {} e {} é {}!'.format(n1, n2, n1+n2))
+print('A soma entre {} e {} é {}!'.format(n1, n2, n1 + n2))
